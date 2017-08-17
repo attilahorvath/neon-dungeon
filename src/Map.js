a064f7b4e0d16776c44be8d762565299c162835c
Attila Horváth <hun.ati500@gmail.com>
2017-08-17 20:04:12 +0100

    Use index buffer to render the map

--- src/Map.js
+++ src/Map.js
@@ -1,12 +1,17 @@
 const NUM_ROOMS = 10;
 const NUM_LINES = 4;
-const NUM_VERTICES = 2;
+const VERTICES_PER_LINE = 2;
+const VERTICES_PER_ROOM = 4;
 const VERTEX_SIZE = 2;
-const ROOM_SIZE = NUM_LINES * NUM_VERTICES * VERTEX_SIZE;
 
 export default class Map {
   constructor(gl) {
-    const vertices = new Float32Array(NUM_ROOMS * ROOM_SIZE);
+    const vertices = new Float32Array(NUM_ROOMS * VERTICES_PER_ROOM *
+                                      VERTEX_SIZE);
+    const indices = new Uint16Array(NUM_ROOMS * NUM_LINES * VERTICES_PER_LINE);
+
+    let verticesIndex = 0;
+    let indicesIndex = 0;
 
     for (let i = 0; i < NUM_ROOMS; i++) {
       const x = Math.random() * 640.0;
@@ -14,34 +19,43 @@ export default class Map {
       const w = 50.0 + Math.random() * 300.0;
       const h = 50.0 + Math.random() * 200.0;
 
-      vertices[i * ROOM_SIZE] = x;
-      vertices[i * ROOM_SIZE + 1] = y;
-      vertices[i * ROOM_SIZE + 2] = x + w;
-      vertices[i * ROOM_SIZE + 3] = y;
+      vertices[verticesIndex++] = x;
+      vertices[verticesIndex++] = y;
 
-      vertices[i * ROOM_SIZE + 4] = x;
-      vertices[i * ROOM_SIZE + 5] = y;
-      vertices[i * ROOM_SIZE + 6] = x;
-      vertices[i * ROOM_SIZE + 7] = y + h;
+      vertices[verticesIndex++] = x + w;
+      vertices[verticesIndex++] = y;
 
-      vertices[i * ROOM_SIZE + 8] = x + w;
-      vertices[i * ROOM_SIZE + 9] = y;
-      vertices[i * ROOM_SIZE + 10] = x + w;
-      vertices[i * ROOM_SIZE + 11] = y + h;
+      vertices[verticesIndex++] = x;
+      vertices[verticesIndex++] = y + h;
 
-      vertices[i * ROOM_SIZE + 12] = x;
-      vertices[i * ROOM_SIZE + 13] = y + h;
-      vertices[i * ROOM_SIZE + 14] = x + w;
-      vertices[i * ROOM_SIZE + 15] = y + h;
+      vertices[verticesIndex++] = x + w;
+      vertices[verticesIndex++] = y + h;
+
+      indices[indicesIndex++] = i * VERTICES_PER_ROOM;
+      indices[indicesIndex++] = i * VERTICES_PER_ROOM + 1;
+
+      indices[indicesIndex++] = i * VERTICES_PER_ROOM;
+      indices[indicesIndex++] = i * VERTICES_PER_ROOM + 2;
+
+      indices[indicesIndex++] = i * VERTICES_PER_ROOM + 1;
+      indices[indicesIndex++] = i * VERTICES_PER_ROOM + 3;
+
+      indices[indicesIndex++] = i * VERTICES_PER_ROOM + 2;
+      indices[indicesIndex++] = i * VERTICES_PER_ROOM + 3;
     }
 
     this.vertexBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, this.vertexBuffer);
     gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
+
+    this.indexBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.indexBuffer);
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, indices, gl.STATIC_DRAW);
   }
 
   draw(gl, basicShader) {
     gl.uniform4f(basicShader.color, 0.0, 0.0, 1.0, 1.0);
-    gl.drawArrays(gl.LINES, 0, NUM_ROOMS * NUM_LINES * NUM_VERTICES);
+    gl.drawElements(gl.LINES, NUM_ROOMS * NUM_LINES * VERTICES_PER_LINE,
+                    gl.UNSIGNED_SHORT, 0);
   }
 }
