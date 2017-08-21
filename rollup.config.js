c369da7b2f0b2aafcaaf40826a2226efcbc1ad53
Attila Horváth <hun.ati500@gmail.com>
2017-08-21 02:03:56 +0100

    Update rollup

--- rollup.config.js
+++ rollup.config.js
@@ -2,9 +2,11 @@ import eslint from 'rollup-plugin-eslint';
 import glsl from 'rollup-plugin-glsl';
 
 export default {
-  entry: 'src/index.js',
-  dest: 'build/index.js',
-  format: 'iife',
+  input: 'src/index.js',
+  output: {
+    file: 'build/index.js',
+    format: 'iife'
+  },
   plugins: [
     eslint({
       exclude: 'shaders/**/*'
